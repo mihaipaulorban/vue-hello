2583eedb534c553cd5c26d61e6c874d6f0748613
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-24 15:19:59 +0100

    feat: aggiunta dell'immagine con createApp

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 
 
-const { createApp } = Vue 
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            messaggio: 'Benvenuto Vue!'
-        }
+            messaggio: 'Benvenuto Vue!',
+            imgPath: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png'
+        };
     }
 }) .mount('#titolo')
